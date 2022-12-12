fe7d99564c789c2ba46abebe857b12d7c733bbfd
Michael Arcidiacono <mixarcidiacono@gmail.com>
2022-12-12 11:47:38 -0500

    added python

--- get_msas.py
+++ get_msas.py
@@ -7,5 +7,5 @@ seen_uniprots = set()
 for i, row in df.iterrows():
     if row.uniprot_id in seen_uniprots: continue
     seen_uniprots.add(row.uniprot_id)
-    subprocess.run("/proj/kuhl_lab/alphafold/run/get_msa.py", "--sequence", row.sequence, "--output-dir", f"data/{row.uniprot_id}")
+    subprocess.run("python", "/proj/kuhl_lab/alphafold/run/get_msa.py", "--sequence", row.sequence, "--output-dir", f"data/{row.uniprot_id}")
     break
