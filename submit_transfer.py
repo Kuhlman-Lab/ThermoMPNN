5aa3800c92d82d54b2ef4835f8cb7e5e97b370b9
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-01-03 09:07:37 -0500

    trying with short thin mlp

--- submit_transfer.py
+++ submit_transfer.py
@@ -8,7 +8,7 @@ from protein_mpnn_utils import loss_nll, loss_smoothed, gather_edges, gather_nod
 
 alphabet = 'ACDEFGHIKLMNPQRSTVWY-'
 def submit(cfg):
-    # model = TransferModelPL.load_from_checkpoint(glob("checkpoints/*.ckpt")[-1], cfg=cfg).model
+    model = TransferModelPL.load_from_checkpoint(glob("checkpoints/*.ckpt")[-1], cfg=cfg).model
 
     pdb = parse_PDB("data/wildtype_structure_prediction_af2.pdb")
     wt_seq = pdb[0]['seq']
@@ -35,18 +35,18 @@ def submit(cfg):
         mutation = Mutation(position=idx, wildtype=wt_aa, mutation=mut_aa, msa_hist=msa_hist)
         mutations.append(mutation)
 
-    preds = []
-    for mut in mutations:
-        if mut is None:
-            preds.append(0.0)
-            continue
-        mut_aa_idx = alphabet.index(mut.mutation)
-        prob = float(mut.msa_hist[mut_aa_idx])
-        print(prob)
-        preds.append(prob)
+    # preds = []
+    # for mut in mutations:
+    #     if mut is None:
+    #         preds.append(0.0)
+    #         continue
+    #     mut_aa_idx = alphabet.index(mut.mutation)
+    #     prob = float(mut.msa_hist[mut_aa_idx])
+    #     print(prob)
+    #     preds.append(prob)
 
-    # with torch.no_grad():
-    #     preds = [ out['ddG'].cpu().item() if out is not None else -100 for out in model(pdb, mutations)[0] ]
+    with torch.no_grad():
+        preds = [ out['ddG'].cpu().item() if out is not None else -100 for out in model(pdb, mutations)[0] ]
 
     df['dTm'] = preds
     new_df = pd.DataFrame({'seq_id': df.seq_id, 'tm': df.dTm })
