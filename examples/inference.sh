24efd3a46207c02431ea233dedc7e162641d64c6
Henry Dieckhaus <dieckhau@longleaf-login5.its.unc.edu>
2024-01-31 09:59:03 -0500

    Added example input/output for custom PDB inference

--- examples/inference.sh
+++ examples/inference.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -J inference
-#SBATCH -t 4:00:00
+#SBATCH -t 30:00:00
 #SBATCH --partition=gpu
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
@@ -16,8 +16,8 @@ conda activate thermoMPNN
 module load gcc
 module load cuda
 
-repo_location="~/ThermoMPNN/"
+repo_location="/proj/kuhl_lab/ThermoMPNN/analysis"
 
 cd $repo_location
 
-python analysis/custom_inference.py --pdb 2OCJ.pdb --chain A --model_path ../models/thermoMPNN_default.pt
+python custom_inference.py --pdb ../examples/2OCJ.pdb --chain A --model_path ../models/thermoMPNN_default.pt
